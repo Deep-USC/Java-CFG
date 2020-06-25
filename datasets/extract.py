3034190a1a90015cfb4ca974322d20a40ceb254b
BohuiZhang <bhzhang97@gmail.com>
2020-06-25 14:18:51 -0700

    Convert to deepwalk input

--- datasets/extract.py
+++ datasets/extract.py
@@ -8,7 +8,7 @@ import os
 import sys
 import json
 
-def parse(input_dir):
+def wrapper(input_dir):
 
     with open(input_dir, 'r') as f:
         functions = json.load(f)
@@ -36,15 +36,50 @@ def parse(input_dir):
             code = "public class Train {\n" + value + "\n}"
         parsed_code[key] = code
         file_name = "Train-" + key + ".java"
-        java_path = os.path.join(os.getcwd(), "java", file_name)
-        with open(java_path, 'x') as f:
+        output_dir = os.path.join(os.getcwd(), "java", file_name)
+        with open(output_dir, 'x') as f:
             f.write(code)
+
+def to_deepwalk_adjlist(input_dir, output_dir): 
+    """
+    Extract and convert CFG in json to `.adjlist`.
+    """
+
+    with open(input_dir, 'r') as f:
+        cfg = json.load(f)
+    
+    node_number = len(cfg['nodes'])
+    adjlist = {}
+    for i in range(node_number):
+        adjlist[i] = [str(i)]
+    for edge in cfg['edges']:
+        adjlist[edge['source']].append(str(edge['target']))
+        adjlist[edge['target']].append(str(edge['source']))
+
+    seperator = " "
+    with open(output_dir, 'x') as f:
+        for i in range(node_number):
+            f.write(seperator.join(adjlist[i]))
+            f.write("\n")
+
+def to_graph2seq(input_dir):
+    """
+    Extract and convert CFG in json to .data`.
+    """
+
+    with open(input_dir, 'r') as f:
+        cfg = json.load(f)
+
+    ...
 
 if __name__ == "__main__":
     #cwd = os.getcwd()  # Get the current working directory (cwd)
     #print(cwd)
     #files = os.listdir(cwd)  # Get all the files in that directory
     #print("Files in %r: %s" % (cwd, files))
-    #path = "C:\Code\DR\datasets\\funcom_processed\\functions.json"
-    path = sys.argv[1]
-    parse(path)
+    #input_path = "C:\Code\DR\DeepUSC\datasets\Test1-CFG.json"
+    #output_path = "C:\Code\DR\DeepUSC\datasets\Test1-CFG.adjlist"
+    input_path = sys.argv[1]
+    output_path = sys.argv[2]
+    #wrapper(input_path)
+    to_deepwalk_adjlist(input_path, output_path)
