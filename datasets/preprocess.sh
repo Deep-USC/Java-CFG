efc766fd0492eb6b4ed2e4dff76f03b3ee5b56e5
BohuiZhang <bhzhang97@gmail.com>
2020-06-29 03:21:44 -0700

    Add extract command

--- datasets/preprocess.sh
+++ datasets/preprocess.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # parse the source code
-python3 extract.py /funcom_processed/functions.json
+python3 extract.py -w -i /funcom_processed/functions.json
 
 # run progex json
 mkdir -p json
@@ -10,3 +10,10 @@ java -jar progex-v3.4.5/progex.jar -outdir ./json -cfg -lang java -format json .
 # run progex dot
 mkdir -p dot
 java -jar progex-v3.4.5/progex.jar -outdir ./dot -cfg -lang java -format dot ./java
+
+# extract CFG
+format = edgelist
+input_dir = json/
+output_dir = json/
+mkdir -p ${format}
+python extract.py -f ${format} -i ${input_dir} -o ${output_dir}
