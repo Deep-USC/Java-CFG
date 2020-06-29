fcea85b4d1c925da0590b5e27367069a5b2e3e7e
BohuiZhang <bhzhang97@gmail.com>
2020-06-29 03:21:05 -0700

    Add edgelist extractor & argparse module

--- datasets/extract.py
+++ datasets/extract.py
@@ -7,42 +7,48 @@
 import os
 import sys
 import json
+import argparse
 
 def wrapper(input_dir):
+    """
+    
+    :param input_dir: the input file path
+    :return:
+    """
 
     with open(input_dir, 'r') as f:
         functions = json.load(f)
     
     parsed_code = {}
 
-    # test case
-    #code = {c: functions[c] for c in list(functions)[:1]}
-    #code = [c for c in list(functions.values())[:1]]
-    #code = list(functions.values())[0]
-    #code = "public class Test {\n" + code + "}"
     count = 0
     
     for key, value in functions.items():
-        #if value[:12] != "public class" or value[:21] != "public abstract class":
         
-        # test first 100 functions since running the whole preprocessing takes time
-        # or one can seperate the json file and running in multiple cmd
+        # TODO: accelerate processing functions.json
         if count == 100:
             break
         count += 1
+        
         if value[0] != "\t" and value[0] != " ":
             code = "public class Train {\n\t" + value + "\n}"
         else:
             code = "public class Train {\n" + value + "\n}"
+        
         parsed_code[key] = code
+        
         file_name = "Train-" + key + ".java"
         output_dir = os.path.join(os.getcwd(), "java", file_name)
         with open(output_dir, 'x') as f:
             f.write(code)
 
-def to_deepwalk_adjlist(input_dir, output_dir): 
+def to_adjlist(input_dir, output_dir): 
     """
     Extract and convert CFG in json to `.adjlist`.
+
+    :param input_dir: the input file path
+    :param output_dir: the output file path
+    :return:
     """
 
     with open(input_dir, 'r') as f:
@@ -62,9 +68,34 @@ def to_deepwalk_adjlist(input_dir, output_dir):
             f.write(seperator.join(adjlist[i]))
             f.write("\n")
 
+def to_edgelist(input_dir, output_dir):
+    """
+    
+    :param input_dir: the input file path
+    :param output_dir: the output file path
+    :return: 
+    """
+
+    with open(input_dir, 'r') as f:
+        cfg = json.load(f)
+
+    edgelist = []
+
+    for edge in cfg['edges']:
+        edgelist.append([str(edge['source']), str(edge['target'])])
+    
+    seperator = " "
+    with open(output_dir, 'x') as f:
+        for edge in edgelist:
+            f.write(seperator.join(edge))
+            f.write("\n")
+
 def to_graph2seq(input_dir):
     """
     Extract and convert CFG in json to .data`.
+
+    :param input_dir: the input file path
+    :return:
     """
 
     with open(input_dir, 'r') as f:
@@ -73,13 +104,23 @@ def to_graph2seq(input_dir):
     ...
 
 if __name__ == "__main__":
-    #cwd = os.getcwd()  # Get the current working directory (cwd)
-    #print(cwd)
-    #files = os.listdir(cwd)  # Get all the files in that directory
-    #print("Files in %r: %s" % (cwd, files))
-    #input_path = "C:\Code\DR\DeepUSC\datasets\Test1-CFG.json"
-    #output_path = "C:\Code\DR\DeepUSC\datasets\Test1-CFG.adjlist"
-    input_path = sys.argv[1]
-    output_path = sys.argv[2]
-    #wrapper(input_path)
-    to_deepwalk_adjlist(input_path, output_path)
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-w", "--wrapper")
+    parser.add_argument("-f", "--format", dest="format", required=True)
+    parser.add_argument("-i", "--input", dest="input_dir", required=True)
+    parser.add_argument("-o", "--output", dest="output_dir", required=True)
+
+    args = parser.parse_args()
+
+    format_func = {
+        "adjlist": to_adjlist,
+        "edgelist": to_edgelist
+    }
+
+    if args.wrapper:
+        wrapper(args.input_dir)
+    
+    if args.format:
+        format_func[args.format](args.input_dir, args.output_dir)
+    
